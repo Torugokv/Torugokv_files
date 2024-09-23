a3b65bcebf50a745216727f8a0df1f18a1baf58e
Torugokv <Vitorugovieira2015@hotmail.com>
2024-09-23 17:02:56 -0400

    23/09/2024
    Dia atual (Vision)

---
+++ Java/CalculadoraIdade.java
@@ -0,0 +1,7 @@
+public class CalculadoraIdade {
+    public static void main(String[] args) {
+        //1-> pegar o ano atual;
+        //2-> pegar o ano de nascimento;
+        //3-> subtrair o ano atual - ano de nascimento;
+    }
+}
