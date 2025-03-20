073c17ce6c384bfc98f97d3613f271376c46bcae
Torugokv <Vitorugovieira2015@hotmail.com>
2025-03-20 12:13:52 -0400

    20-03
    Daily Study

--- Javascript/exemplo.js
+++
@@ -1,7 +0,0 @@
-function exemploVar() {
-    if (true) {
-        var x = 10;
-        console.log(x); // 10
-    }
-    console.log(x); // 10 
-}
---
+++ Javascript/main.js
@@ -0,0 +1,3 @@
+let message = "Hello World!";
+
+message = "Olá Mundo!";
