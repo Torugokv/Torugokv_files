01531f17a0ec0b417fab2597a0dca197b7d7bc2c
Torugokv <Vitorugovieira2015@hotmail.com>
2025-03-21 17:04:47 -0400

    21/03
    Aaaaa

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,24 @@
-let message = "Hello World!";
+const numbers = [1, 2, 3, 4, 5];
 
-message = "Olá Mundo!";
+const numbersMultipliedbyTwo = numbers.map(function (number) {
+  return number * 2;
+});
+
+console.log(numbersMultipliedbyTwo);
+
+const ages = [8, 13, 27, 30, 22, 40];
+
+const evenAges = ages.filter(function (age) {
+  return age % 2 === 0;
+});
+
+console.log(evenAges);
+
+const sumOfAges = ages.reduce(function () {});
+
+const person = {
+  firstName: "Felipe",
+  lastName: "Rocha",
+  age: "21",
+  hobbies: ["Assistir F1", "Jogar futebol", "Ler"],
+};
